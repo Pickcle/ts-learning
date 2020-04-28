d4b14078972c31ef965372be66c54aaa63a14578
xiaohuajun <xiaohuajun.pickcle@bytedance.com>
2020-04-28 18:51:37 +0800

    3 2

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var minus = require('./minus.js')
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 var master = 4, dev = 2
 =======
 var master = 2, dev = 3
@@ -12,6 +13,9 @@ var master = 2, dev = 3
 =======
 var master = 2, dev = 4
 >>>>>>> 2 4
+=======
+var master = 3, dev = 4
+>>>>>>> 3 2
 plus(master, dev)
 minus(master, dev)
 // var res2 = minus(3,10)
