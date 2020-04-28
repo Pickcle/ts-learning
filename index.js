10fb2e24bf29749013b5804b8d599b9687c9e295
xiaohuajun <xiaohuajun.pickcle@bytedance.com>
2020-04-28 18:04:35 +0800

    first commit on master

---
+++ index.js
@@ -0,0 +1,8 @@
+var plus = require('./plus.js')
+var minus = require('./minus.js')
+
+console.log('this is first commit on master')
+
+var res1 = plus(1,2)
+var res2 = minus(3,1)
+console.log(res1, res2)
---
+++ minus.js
@@ -0,0 +1,5 @@
+function minus(num1, num2) {
+  return num2 - num1
+}
+
+module.exports = exports = minus
