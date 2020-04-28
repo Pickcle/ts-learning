4b05cc2d7e18a05390e114e6ac250eac7da2d1c7
xiaohuajun <xiaohuajun.pickcle@bytedance.com>
2020-04-28 18:51:21 +0800

    2 4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var minus = require('./minus.js')
 
 // console.log('this is second commit on master')
 
-var master = 2, dev = 3
+var master = 2, dev = 4
 plus(master, dev)
 minus(master, dev)
 // var res2 = minus(3,10)
