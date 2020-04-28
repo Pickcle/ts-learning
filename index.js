6e0b62ada63387bda6011ae1bbdbebd6facbefaa
xiaohuajun <xiaohuajun.pickcle@bytedance.com>
2020-04-28 21:05:18 +0800

    delete verbose code

--- index.js
+++ index.js
@@ -3,27 +3,7 @@ var minus = require('./minus.js')
 
 // console.log('this is second commit on master')
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-var master = 4, dev = 2
-=======
-var master = 2, dev = 3
->>>>>>> 2 3
-=======
-var master = 2, dev = 4
->>>>>>> 2 4
-=======
-var master = 3, dev = 4
->>>>>>> 3 2
-=======
 var master = 4, dev = 4
->>>>>>> 4 2
-=======
-var master = 4, dev = 4
->>>>>>> 43cc873dca2422c448ccedc6e2a31390c3e98a29
 plus(master, dev)
 minus(master, dev)
 // var res2 = minus(3,10)
