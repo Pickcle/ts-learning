f038c80e82b9fdee20d2a75ed212bc81b9b25f8b
xiaohuajun <xiaohuajun.pickcle@bytedance.com>
2020-04-28 18:13:40 +0800

    second commit on master

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var plus = require('./plus.js')
 var minus = require('./minus.js')
 
-console.log('this is first commit on master')
+console.log('this is second commit on master')
 
 var res1 = plus(1,2)
-var res2 = minus(3,1)
+var res2 = minus(3,10)
 console.log(res1, res2)
